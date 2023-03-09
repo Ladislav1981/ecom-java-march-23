4d1cffc7b89ba3cdc50b47304640bf4582d8226a
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-09 21:58:23 +0300

    Update First.java

--- src/Git_practice/First.java
+++ src/Git_practice/First.java
@@ -3,7 +3,7 @@ package Git_practice;
 
 public class First {
     public static void main(String[] args) {
-        int MyInt=20;
+        int MyInt=40;
 
         System.out.println("MyInt = " + MyInt);
     }
