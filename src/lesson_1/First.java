097628824d8c0748fb160a31da3b5e45c993bac3
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-10 12:04:54 +0300

    Update First.java

--- src/lesson_1/First.java
+++ src/lesson_1/First.java
@@ -3,8 +3,27 @@ package lesson_1;
 
 public class First {
     public static void main(String[] args) {
+        byte MyByte=17;
+        short MyShort=460;
         int MyInt=40;
+        long MyLong=1000000L;
+        float MyFloat=560.232F;
+        double MyDouble=454545454.5;
+        char MyChar='F';
+        String MyString="Ladislav";
+        boolean MyBoolean = false;
+        boolean MyBoolean1= true;
 
+        System.out.println("MyByte = " + MyByte);
+        System.out.println("MyShort = " + MyShort);
         System.out.println("MyInt = " + MyInt);
+        System.out.println("MyLong = " + MyLong);
+        System.out.println("MyFloat = " + MyFloat);
+        System.out.println("MyDouble = " + MyDouble);
+        System.out.println("MyChar = " + MyChar);
+        System.out.println("MyString = " + MyString);
+        System.out.println("MyBoolean1 = " + MyBoolean1);
+        System.out.println("MyBoolean = " + MyBoolean);
+
     }
 }
