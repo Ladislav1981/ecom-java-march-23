97ef5606145885c60f6268cb94752e57361cec68
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-13 20:26:01 +0300

    assigment

---
+++ src/lesson_3/assigment_operators.java
@@ -0,0 +1,33 @@
+package lesson_3;
+
+public class assigment_operators {
+    public static void main(String[] args) {
+
+        var a=30; //optional decloration of variable//
+
+       a = 50;
+
+
+
+
+        a+=5; //тоже самое как a=a+5//
+        System.out.println("a = " +a);
+
+
+        a-=5; //тоже самое как a=a-5//
+        System.out.println("a = "+a);
+
+
+        a*=5; //тоже самое как a=a*5//
+        System.out.println("a = "+a);
+
+
+        a/=5; //тоже самое как a=a/5//
+        System.out.println("a = "+a);
+
+
+        a%=5; //тоже самое как a=a%5//
+        System.out.println("a = "+a);
+
+    }
+}
