a5b62e90c72889c24372a1a4bdbce19885010114
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-13 19:28:15 +0300

    Create Modulo_div.java

---
+++ src/lesson_3/Modulo_div.java
@@ -0,0 +1,13 @@
+package lesson_3;
+
+public class Modulo_div {
+    public static void main(String[] args) {
+
+        int a =3687;
+
+        System.out.println(a + " % " + 10 + " = " + (a%10));
+        System.out.println(a + " % " + 100 + " = " + (a%100));
+        System.out.println(a + " / " + 10 + " = " + (a/10));
+        System.out.println(a + " / " + 100 + " = " + (a/100));
+    }
+}
