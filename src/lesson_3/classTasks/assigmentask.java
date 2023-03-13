bcb058090cf90d11eda3e97b95573485e7d2ba4d
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-13 20:26:20 +0300

    task

---
+++ src/lesson_3/classTasks/assigmentask.java
@@ -0,0 +1,18 @@
+package lesson_3.classTasks;
+
+public class assigmentask {
+
+    public static void main(String[] args) {
+
+
+        int a =10;
+        a+=90;
+        a-=25;
+        a/=3;
+        a%=10;
+        a*=(-3);
+        a=a++;
+        a=a--;
+        System.out.println("a = " +a);
+    }
+}
