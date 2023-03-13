c588943b5882e105c3fe90393ceae249762b29a6
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-13 20:26:37 +0300

    increment decrement

---
+++ src/lesson_3/increment_decrement.java
@@ -0,0 +1,34 @@
+package lesson_3;
+
+public class increment_decrement {
+    public static void main(String[] args) {
+
+
+          int a =10;
+
+        System.out.println("a=  " +a) ;
+
+      //  a=a+1;
+      //  a+=1;
+        //increment//
+        System.out.println("a++ =" + a++);
+        System.out.println("a++ =" + a);
+        System.out.println("a++ =" + ++ a); //это как 11+a//
+        System.out.println("a++ =" + a);
+
+
+         //decrement//
+        System.out.println("a=  " +a) ;
+
+        //  a=a-1;
+        //  a-=1;
+
+        System.out.println("a-- =" + a--);
+        System.out.println("a =" + a);
+        System.out.println("--a =" + -- a); //это как 11+a//
+        System.out.println("a =" + a);
+
+
+
+    }
+}
