368293409fc8de6911869181b130b3b42a96d0f7
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-14 08:40:17 +0300

    Check_Numbers_Task

---
+++ src/homeTasks/CheckNumbers.java
@@ -0,0 +1,22 @@
+package homeTasks;
+
+public class CheckNumbers {
+    public static void main(String[] args) {
+
+        //Целые числа которые делятся на 2 без остатка считаются четными
+        //Все числа которые делятся на 2 с остатком считаются не четными.//
+
+
+        System.out.println("Modulo of 10%2->>>" + (10%2==0));
+
+        System.out.println("Modulo of 25%2->>> "+ (25%2==0));
+
+        System.out.println("Modulo of 55%2->>> "+(55%2==0));
+
+        System.out.println("Modulo of 22%2->>>"+(22%2==0));
+
+        System.out.println(" Modulo of 0%2->>>" + (0%2==0));
+
+
+    }
+}
