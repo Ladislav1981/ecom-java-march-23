cafa450ff9ccaa32298d1c439ecde25a72442b6d
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-14 17:45:05 +0300

    Update ScannerHomeTask.java

--- src/homeTasks/ScannerHomeTask.java
+++ src/homeTasks/ScannerHomeTask.java
@@ -4,18 +4,19 @@ public class ScannerHomeTask {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
         System.out.println("What is your First name? ");
         String myFirstName= input.nextLine();
-
         System.out.println("My First Name = " + myFirstName);
+
         System.out.println("What is your Last Name?");
         String myLastName= input.nextLine();
-
         System.out.println("My Last Name =" +myLastName );
+
         System.out.println("Where do You live?");
         String myCity= input.nextLine();
-
         System.out.println("I Live in =" +myCity );
+
         System.out.println("Where do You Study?");
         String myStudy= input.nextLine();
         System.out.println("I Study in =" +myStudy );
