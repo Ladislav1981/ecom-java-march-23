0154eb1c1ce0acfe9f8ca13b590d120c538a5479
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-20 18:09:49 +0300

    Update Arrays.java

---
+++ src/lesson_5/ArrayClassTask/Arrays.java
@@ -0,0 +1,16 @@
+package lesson_5.ArrayClassTask;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        int numbersArray [] = new int[5];
+        numbersArray[0]=30;
+
+        System.out.println(numbersArray [0]);
+
+        int numbersOfarrays[] ={5,10,15,20};
+
+        System.out.println(numbersOfarrays[2]);
+        System.out.println(numbersOfarrays.length);
+    }
+}
--- src/lesson_5/ClassTask/loops.java
+++
@@ -1,7 +0,0 @@
-package lesson_5.ClassTask;
-
-public class loops {
-    public static void main(String[] args) {
-        
-    }
-}
