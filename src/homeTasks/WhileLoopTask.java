6e80d3fd011fcff4cfd5fd51829021dd29ecb797
Ladislav1981 <126403743+Ladislav1981@users.noreply.github.com>
2023-03-23 17:19:42 +0300

    Update WhileLoopTask.java

--- src/homeTasks/WhileLoopTask.java
+++ src/homeTasks/WhileLoopTask.java
@@ -5,31 +5,27 @@ public class WhileLoopTask {
 
 
         int j = 0;
-        int sum=0;
+        int sum = 0;
 
         while (j <= 500) {
 
-            if (j % 7 == 0){
+            if(j%7==0){
                 System.out.println(j);
-                sum=j+sum;
                 j++;
                 continue;
             }
 
+            if(j%4==0){
 
+            }
 
-            if (j == 100) {
-
+            if(j==100){
                 break;
             }
 
             j++;
 
         }
-
-
-        System.out.println(sum);
-
-
     }
+
 }
